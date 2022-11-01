a65a0390d7d716033ff9425eed0ece7028146f72
Kristóf <115035960+SoldierK@users.noreply.github.com>
2022-11-01 17:22:44 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#import math
+import sajat_modul
+from math import sqrt, pi
+
+p = 3.1415
+
+print(p)
+print(pi)
+print(sajat_modul.pi)
+
+print(sqrt(9))
+
+print(sajat_modul.osszeg(5, 15))
+
+print(sajat_modul.bevitel('sz'))
